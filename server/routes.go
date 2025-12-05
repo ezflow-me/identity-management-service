399a387d68848b7c54ab539c14ea94fdd8d746d0
juanca <juancarloslucenamonje@gmail.com>
2025-12-05 11:07:04 +0000

    feat: add user routes to server

--- server/routes.go
+++ server/routes.go
@@ -1,10 +1,14 @@
 package server
 
 import (
+	"github.com/ezflow-me/identity-management-service/src/contexts/identity-access/users/infrastructure/router"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
 func SetupRoutes(app *fiber.App) {
 	app.Get("/metrics", monitor.New())
+
+	user := app.Group("/user")
+	router.Router(user)
 }
